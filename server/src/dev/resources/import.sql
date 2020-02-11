a02e0238be8ea06053746de2754b7f13fc1a057e
Miro Spönemann <miro.spoenemann@typefox.io>
2020-02-11 13:13:40 +0000

    [server] Added spring-session-jdbc

--- server/src/dev/resources/import.sql
+++ server/src/dev/resources/import.sql
@@ -1,50 +1,50 @@
 -- Create a super user with a personal access token `super_token` for publishing with the CLI.
-INSERT INTO user_data (id, login_name) VALUES (1, 'super_user');
-INSERT INTO personal_access_token (id, user_data, value, created_timestamp, accessed_timestamp, description) VALUES (1, 1, 'super_token', current_timestamp, current_timestamp, 'For publishing test extensions');
+INSERT INTO user_data (id, login_name) VALUES (1001, 'super_user');
+INSERT INTO personal_access_token (id, user_data, value, created_timestamp, accessed_timestamp, description) VALUES (1001, 1001, 'super_token', current_timestamp, current_timestamp, 'For publishing test extensions');
 
 -- Create all required publishers for the downloaded test extensions and assign `super_user` as owner.
 
-INSERT INTO publisher (id, name) VALUES (1, 'DotJoshJohnson');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1, 1, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1001, 'DotJoshJohnson');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1001, 1001, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (2, 'eamodio');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (2, 2, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1002, 'eamodio');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1002, 1002, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (3, 'Equinusocio');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (3, 3, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1003, 'Equinusocio');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1003, 1003, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (4, 'felixfbecker');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (4, 4, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1004, 'felixfbecker');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1004, 1004, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (5, 'formulahendry');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (5, 5, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1005, 'formulahendry');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1005, 1005, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (6, 'HookyQR');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (6, 6, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1006, 'HookyQR');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1006, 1006, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (7, 'ms-azuretools');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (7, 7, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1007, 'ms-azuretools');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1007, 1007, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (8, 'ms-mssql');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (8, 8, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1008, 'ms-mssql');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1008, 1008, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (9, 'ms-python');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (9, 9, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1009, 'ms-python');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1009, 1009, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (10, 'ms-vscode');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (10, 10, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1010, 'ms-vscode');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1010, 1010, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (11, 'octref');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (11, 11, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1011, 'octref');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1011, 1011, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (12, 'redhat');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (12, 12, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1012, 'redhat');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1012, 1012, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (13, 'ritwickdey');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (13, 13, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1013, 'ritwickdey');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1013, 1013, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (14, 'sburg');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (14, 14, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1014, 'sburg');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1014, 1014, 1001, 'owner');
 
-INSERT INTO publisher (id, name) VALUES (15, 'Wscats');
-INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (15, 15, 1, 'owner');
+INSERT INTO publisher (id, name) VALUES (1015, 'Wscats');
+INSERT INTO publisher_membership (id, publisher, user_data, role) VALUES (1015, 1015, 1001, 'owner');
