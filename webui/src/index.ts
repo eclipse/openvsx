adeef072f83240d4b5c812f10c5d8e6d9964fce2
Miro Spönemann <miro.spoenemann@typefox.io>
2020-03-31 16:54:30 +0000

    Added index.ts to openvsx-webui package

---
+++ webui/src/index.ts
@@ -0,0 +1,17 @@
+/********************************************************************************
+ * Copyright (c) 2020 TypeFox and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+export * from './main';
+export * from './page-settings';
+export * from './extension-registry-service';
+export * from './extension-registry-types';
+export * from './pages/extension-detail/extension-detail';
+export * from './pages/extension-list/extension-list';
+export * from './pages/user/user-settings';
