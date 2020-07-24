ad064e5c9d08e3d519c7e5dd5ad07b1705c56d98
Miro Spönemann <miro.spoenemann@typefox.io>
2020-07-24 10:07:00 +0000

    Added publishing extensions to integration test

--- server/src/test/java/org/eclipse/openvsx/IntegrationTest.java
+++ server/src/test/java/org/eclipse/openvsx/IntegrationTest.java
@@ -11,26 +11,72 @@ package org.eclipse.openvsx;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.io.ByteStreams;
+
+import org.eclipse.openvsx.json.ExtensionJson;
+import org.eclipse.openvsx.json.NamespaceJson;
+import org.eclipse.openvsx.json.ResultJson;
+import org.eclipse.openvsx.repositories.RepositoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
     @LocalServerPort
-	private int port;
+	int port;
 
 	@Autowired
-	private TestRestTemplate restTemplate;
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    RepositoryService repositories;
+
+    @Autowired
+    TestService testService;
 
 	@Test
 	public void testEmptySearch() throws Exception {
-        var response = restTemplate.getForObject("http://localhost:" + port + "/api/-/search", String.class);
-		assertThat(response).isEqualTo("{\"offset\":0,\"totalSize\":0,\"extensions\":[]}");
-	}
+        var response = restTemplate.getForEntity("http://localhost:" + port + "/api/-/search", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("{\"offset\":0,\"totalSize\":0,\"extensions\":[]}");
+    }
+    
+    @Test
+    public void testPublishExtension() throws Exception {
+        testService.createUser();
+
+        // Create the namespace
+        var requestBody = new NamespaceJson();
+        requestBody.name = "Equinusocio";
+        var response1 = restTemplate.postForEntity("http://localhost:" + port + "/api/-/namespace/create?token={token}",
+                    requestBody, ResultJson.class, "test_token");
+        assertThat(response1.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response1.getBody().error).isNull();
+        assertThat(response1.getBody().success).isEqualTo("Created namespace " + requestBody.name);
+
+        try (
+            var stream = getClass().getResourceAsStream("vsc-material-theme.vsix");
+        ) {
+            // Publish the extension
+            var bytes = ByteStreams.toByteArray(stream);
+            var response2 = restTemplate.postForEntity("http://localhost:" + port + "/api/-/publish?token={token}",
+                    bytes, ExtensionJson.class, "test_token");
+            assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response2.getBody().error).isNull();
+            assertThat(response2.getBody().name).isEqualTo("vsc-material-theme");
+        }
+
+        // Query the metadata of the published extension
+        var response3 = restTemplate.getForEntity("http://localhost:" + port + "/api/Equinusocio/vsc-material-theme",
+                ExtensionJson.class);
+        assertThat(response3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response3.getBody().description).isEqualTo("The most epic theme now for Visual Studio Code");
+    }
     
 }
---
+++ server/src/test/java/org/eclipse/openvsx/TestService.java
@@ -0,0 +1,41 @@
+/********************************************************************************
+ * Copyright (c) 2020 TypeFox and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+package org.eclipse.openvsx;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.eclipse.openvsx.entities.PersonalAccessToken;
+import org.eclipse.openvsx.entities.UserData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestService {
+    
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public void createUser() {
+        var user = new UserData();
+        user.setLoginName("test_user");
+        entityManager.persist(user);
+        var token = new PersonalAccessToken();
+        token.setCreatedTimestamp(LocalDateTime.now());
+        token.setActive(true);
+        token.setUser(user);
+        token.setValue("test_token");
+        entityManager.persist(token);
+    }
+    
+}
