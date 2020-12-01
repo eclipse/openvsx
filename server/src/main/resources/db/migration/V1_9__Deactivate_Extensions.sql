93e31b9e8cc03801d9e8cbcb125eb07480bda256
Miro Spönemann <miro.spoenemann@typefox.io>
2020-12-01 11:31:25 +0000

    Fixed DB migration v1.9

--- server/src/main/resources/db/migration/V1_9__Deactivate_Extensions.sql
+++ server/src/main/resources/db/migration/V1_9__Deactivate_Extensions.sql
@@ -1,11 +1,17 @@
 ALTER TABLE public.extension
-    ADD active boolean NOT NULL;
+    ADD active boolean;
 
 UPDATE public.extension
     SET active = true;
 
+ALTER TABLE public.extension
+    ALTER COLUMN active SET NOT NULL;
+
 ALTER TABLE public.extension_version
-    ADD active boolean NOT NULL;
+    ADD active boolean;
 
 UPDATE public.extension_version
     SET active = true;
+
+ALTER TABLE public.extension_version
+    ALTER COLUMN active SET NOT NULL;
