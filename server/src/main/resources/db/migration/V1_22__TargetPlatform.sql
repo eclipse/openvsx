601a78932dc5ee9a60cfd7e7ab94f30772524be2
amvanbaren <amvanbaren@hotmail.com>
2022-04-04 23:22:26 +0200

    Include extension_id in unique_extension_version constraint

--- server/src/main/resources/db/migration/V1_22__TargetPlatform.sql
+++ server/src/main/resources/db/migration/V1_22__TargetPlatform.sql
@@ -6,5 +6,5 @@ UPDATE extension_version SET target_platform = 'universal';
 ALTER TABLE extension_version ALTER COLUMN target_platform SET NOT NULL;
 
 DROP INDEX unique_extension_version;
-CREATE UNIQUE INDEX unique_extension_version_idx ON extension_version(target_platform, version);
+CREATE UNIQUE INDEX unique_extension_version_idx ON extension_version(extension_id, target_platform, version);
 ALTER TABLE extension_version ADD CONSTRAINT unique_extension_version UNIQUE USING INDEX unique_extension_version_idx;
