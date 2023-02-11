11b13a113ccb315c80d02e2e371f11b881af1655
Filip Troníček <filip.tronicek@seznam.cz>
2023-02-11 14:28:59 +0100

    Better error handling in the CLI

--- cli/node-ovsx-sign/src/index.ts
+++ cli/node-ovsx-sign/src/index.ts
@@ -1,5 +1,5 @@
 import * as commander from "commander";
-import { verify, sign } from "./utils/sign";
+import { verify, sign, ExtensionSignatureVerificationError } from "./utils/sign";
 
 export default function (argv: string[]): void {
   const program = new commander.Command();
@@ -10,8 +10,17 @@ export default function (argv: string[]): void {
     .description("Verify an extension package")
     .arguments("<extension-package> <signature-archive>")
     .option("-p, --public-key <public-key>", "The path to the public key to use for verification")
-    .action((vsixFilePath: string, signatureArchiveFilePath: string, { publicKey }) => {
-      verify(vsixFilePath, signatureArchiveFilePath, { publicKey });
+    .action(async (vsixFilePath: string, signatureArchiveFilePath: string, { publicKey }) => {
+      try {
+        await verify(vsixFilePath, signatureArchiveFilePath, { publicKey });
+      } catch (e) {
+        if (e instanceof ExtensionSignatureVerificationError) {
+          process.exit(e.code);
+        } else {
+          console.error(e.message);
+          process.exit(1);
+        }
+      }
     });
 
   const signCmd = program.command("sign");
