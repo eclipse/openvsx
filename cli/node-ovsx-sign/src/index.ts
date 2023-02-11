38461b4463acaa07b49e29d51df0324073aa91d2
Filip Troníček <filip.tronicek@seznam.cz>
2023-02-11 14:25:47 +0100

    Rewrite using Node crypto

--- cli/node-ovsx-sign/src/index.ts
+++ cli/node-ovsx-sign/src/index.ts
@@ -1,81 +1,5 @@
 import * as commander from "commander";
-import * as fs from "fs";
-import * as crypto from "crypto";
-import { exec } from "./utils/exec";
-import { downloadPublicKey } from "./utils/downloadPublicKey";
-
-const SIGNED_ARCHIVE_NAME = "extension.sigzip";
-const HASHED_PACKAGE_NAME = "extension.vsix.hash";
-
-export class ExtensionSignatureVerificationError extends Error {
-  code: number;
-  didExecute: boolean;
-  constructor(code: number, didExecute: boolean) {
-    super();
-    this.code = code;
-    this.didExecute = didExecute;
-  }
-}
-
-/**
- * Verify an extension package against a signature archive
- * @param vsixFilePath The extension file path.
- * @param signatureArchiveFilePath The signature archive file path.
- * @throws { ExtensionSignatureVerificationError } An error with a code indicating the validity, integrity, or trust issue
-   * found during verification or a more fundamental issue (e.g.:  a required dependency was not found).
- */
-export const verify = async (vsixFilePath: string, signatureArchiveFilePath: string, options?: {
-  publicKey?: string;
-}): Promise<boolean> => {
-
-  if (!fs.existsSync(vsixFilePath)) {
-    throw new ExtensionSignatureVerificationError(3, false);
-  }
-
-  if (!fs.existsSync(signatureArchiveFilePath)) {
-    throw new ExtensionSignatureVerificationError(6, false);
-  }
-
-  const extensionFile = await fs.promises.readFile(vsixFilePath);
-  const publicKeyLocation = options?.publicKey || await downloadPublicKey();
-
-  const hashOfExtensionFile = crypto.createHash("sha256").update(extensionFile).digest("hex");
-  await fs.promises.writeFile(`./${HASHED_PACKAGE_NAME}`, hashOfExtensionFile);
-
-  await exec(`openssl pkeyutl -verify -pubin -inkey ${publicKeyLocation} -sigfile ${signatureArchiveFilePath} -in ./${HASHED_PACKAGE_NAME}`, {}).catch((err) => {
-    console.error(err);
-    if (err.message.includes("Can't open signature file")) {
-      throw new ExtensionSignatureVerificationError(7, true);
-    } else if (err.message.includes("Signature Verification Failure")) {
-      throw new ExtensionSignatureVerificationError(102, true);
-    }
-    throw new ExtensionSignatureVerificationError(110, true);
-  });
-
-  // Cleanup
-  await fs.promises.unlink(`./${HASHED_PACKAGE_NAME}`);
-
-  return true;
-};
-
-/**
- * Sign an extension package. The signature is saved to `extension.sigzip`
- * @param vsixFilePath the path to the `.vsix` file of the extension
- * @param privateKeyFilePath the path to the private key used to sign the extension
- */
-export const sign = async (vsixFilePath: string, privateKeyFilePath: string): Promise<void> => {
-  const extensionFile = await fs.promises.readFile(vsixFilePath);
-
-  const extensionPackageHash = crypto.createHash("sha256").update(extensionFile).digest("hex");
-  await fs.promises.writeFile(`./${HASHED_PACKAGE_NAME}`, extensionPackageHash);
-
-  await exec(`openssl pkeyutl -sign -inkey ${privateKeyFilePath} -in ./${HASHED_PACKAGE_NAME} -out ${SIGNED_ARCHIVE_NAME}`, {});
-
-  // Cleanup
-  await fs.promises.unlink(`./${HASHED_PACKAGE_NAME}`);
-
-  console.info(`Signature file created at ./${SIGNED_ARCHIVE_NAME}`);
-};
+import { verify, sign } from "./utils/sign";
 
 export default function (argv: string[]): void {
   const program = new commander.Command();
@@ -94,6 +18,7 @@ export default function (argv: string[]): void {
   signCmd
     .description("Sign an extension package")
     .arguments("<extension-package> <private-key>")
+    .option("-o, --output <output>", "The path to the output signature archive")
     .action(sign);
 
   program.parse(argv);
---
+++ cli/node-ovsx-sign/src/utils/sign.ts
@@ -0,0 +1,79 @@
+import * as crypto from 'crypto';
+import * as fs from "fs";
+import { downloadPublicKey, loadPrivateKey, loadPublicKey } from './keys';
+
+export const SIGNED_ARCHIVE_NAME = "extension.sigzip";
+export const HASHED_PACKAGE_NAME = "extension.vsix.hash";
+
+export class ExtensionSignatureVerificationError extends Error {
+    code: number;
+    didExecute: boolean;
+    constructor(code: number, didExecute: boolean) {
+        super();
+        this.code = code;
+        this.didExecute = didExecute;
+    }
+}
+
+const signFile = async (file: Buffer, privateKey: string): Promise<Buffer> => {
+    const sign = crypto.createSign('sha256');
+    sign.update(file);
+    return sign.sign(privateKey);
+}
+
+const verifySignature = async (file: Buffer, publicKey: string, signature: Buffer): Promise<boolean> => {
+    const verify = crypto.createVerify('sha256');
+    verify.update(file);
+    return verify.verify(publicKey, signature);
+}
+
+/**
+ * Sign an extension package. The signature is saved to `extension.sigzip`
+ * @param vsixFilePath the path to the `.vsix` file of the extension
+ * @param privateKeyFilePath the path to the private key used to sign the extension
+ */
+export const sign = async (vsixFilePath: string, privateKeyFilePath: string, options?: {
+    output?: string;
+}): Promise<void> => {
+    const extensionFile = await fs.promises.readFile(vsixFilePath);
+    const privateKey = await loadPrivateKey(privateKeyFilePath);
+    const outputPath = options?.output ?? `./${SIGNED_ARCHIVE_NAME}`
+
+    const signature = await signFile(extensionFile, privateKey);
+    await fs.promises.writeFile(outputPath, signature);
+
+    console.info(`Signature file created at ${outputPath}`);
+};
+
+/**
+* Verify an extension package against a signature archive
+* @param vsixFilePath The extension file path.
+* @param signatureArchiveFilePath The signature archive file path.
+* @throws { ExtensionSignatureVerificationError } An error with a code indicating the validity, integrity, or trust issue
+ * found during verification or a more fundamental issue (e.g.:  a required dependency was not found).
+*/
+export const verify = async (vsixFilePath: string, signatureArchiveFilePath: string, options?: {
+    publicKey?: string;
+}): Promise<boolean> => {
+
+    if (!fs.existsSync(vsixFilePath)) {
+        throw new ExtensionSignatureVerificationError(3, false);
+    }
+
+    if (!fs.existsSync(signatureArchiveFilePath)) {
+        throw new ExtensionSignatureVerificationError(6, false);
+    }
+
+    const extensionFile = await fs.promises.readFile(vsixFilePath);
+    const publicKey = await loadPublicKey(options?.publicKey || await downloadPublicKey());
+    const signature = await fs.promises.readFile(signatureArchiveFilePath);
+    const signatureValid = await verifySignature(extensionFile, publicKey, signature);
+
+    if (!signatureValid) {
+        console.error("Signature is not valid");
+        throw new ExtensionSignatureVerificationError(102, true);
+    }
+
+    console.info("Signature is valid");
+    return true;
+};
