d18e5392f4e79d91f7e8f37889d71d0add393f8b
Thomas Neidhart <thomas.neidhart@gmail.com>
2025-12-02 08:20:18 +0100

    cleanup imports

--- server/src/main/java/org/eclipse/openvsx/storage/log/DownloadCountProcessor.java
+++ server/src/main/java/org/eclipse/openvsx/storage/log/DownloadCountProcessor.java
@@ -28,8 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.eclipse.openvsx.entities.FileResource.STORAGE_AZURE;
-
 @Component
 public class DownloadCountProcessor {
 
