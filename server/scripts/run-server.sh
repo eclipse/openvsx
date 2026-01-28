8db26facf0fb8391bbc7637769d99de94132dae2
Thomas Neidhart <thomas.neidhart@gmail.com>
2026-01-28 20:02:34 +0100

    chore: revert quoting of JVM_ARGS

--- server/scripts/run-server.sh
+++ server/scripts/run-server.sh
@@ -3,4 +3,5 @@
 # This script is included in the Docker image to run the server application
 echo "JVM_ARGS: '${JVM_ARGS}'"
 
-exec java "${JVM_ARGS}" -cp BOOT-INF/classes:BOOT-INF/lib/* org.eclipse.openvsx.RegistryApplication
+# shellcheck disable=SC2086
+exec java ${JVM_ARGS} -cp BOOT-INF/classes:BOOT-INF/lib/* org.eclipse.openvsx.RegistryApplication
