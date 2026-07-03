76693ad66d179d77ecf8e88b9ff9bdb1575eef3d
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2026-07-03 12:19:53 +0200

    chore: use full license headers

--- webui/src/components/extension-list.tsx
+++ webui/src/components/extension-list.tsx
@@ -1,8 +1,16 @@
-/******************************************************************************
- * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+/********************************************************************************
+ * Copyright (c) 2019 TypeFox and others
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
- *****************************************************************************/
+ ********************************************************************************/
 
 import { FunctionComponent, useContext, useEffect, useRef, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
