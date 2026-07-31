d10acaf7d3d8599e241037d95a95fb4e45abe62d
Jordi Gómez <jordi.gomez@eclipse-foundation.org>
2026-07-31 13:33:12 +0200

    feat: exposing analytics enabled flag on version endpoint

--- server/src/main/java/org/eclipse/openvsx/LocalRegistryService.java
+++ server/src/main/java/org/eclipse/openvsx/LocalRegistryService.java
@@ -135,6 +135,9 @@ public class LocalRegistryService implements IExtensionRegistry {
     @Value("${ovsx.registry.version:}")
     String registryVersion;
 
+    @Value("${ovsx.analytics.enabled:false}")
+    boolean analyticsEnabled;
+
     @Override
     public NamespaceJson getNamespace(String namespaceName) {
         return getNamespace(namespaceName, false);
@@ -1378,6 +1381,7 @@ public class LocalRegistryService implements IExtensionRegistry {
         json.setMaxExtensionSize(publishingConfig.getMaxContentSize());
         json.setTrustedPublishingAudience(
                 trustedPublishingConfig.isEnabled() ? trustedPublishingConfig.getAudience() : null);
+        json.setAnalyticsEnabled(analyticsEnabled);
         return json;
     }
 
