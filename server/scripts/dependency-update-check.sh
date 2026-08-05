593c1d561bdd32d9ebd1fc07d58fe108501ed076
Tamas Cservenak <tamas@cservenak.net>
2026-08-05 23:12:04 +0200

    Use new --force-updates

--- server/scripts/dependency-update-check.sh
+++ server/scripts/dependency-update-check.sh
@@ -9,7 +9,7 @@ cd "${SERVER_ROOT}"
 
 ./gradlew listDependencies --quiet
 
-jbang toolbox@maveniverse versions build/dependencies/list.txt --artifactVersionSelectorSpec="minor()" | grep -v "up to date" > gradle/dependency-updates.txt
+jbang toolbox@maveniverse versions --force-updates build/dependencies/list.txt --artifactVersionSelectorSpec="minor()" | grep -v "up to date" > gradle/dependency-updates.txt
 
 if git diff --exit-code --quiet gradle/dependency-updates.txt; then
     echo "All dependencies are up to date."
