d3b149ec493c7f3c3d86cbafc656c8c8b3c689c9
Tamas Cservenak <tamas@cservenak.net>
2026-08-21 12:38:47 +0200

    Regenerate jOOQ classes.

--- server/src/main/jooq-gen/org/eclipse/openvsx/jooq/tables/ExtensionVersion.java
+++ server/src/main/jooq-gen/org/eclipse/openvsx/jooq/tables/ExtensionVersion.java
@@ -130,11 +130,6 @@ public class ExtensionVersion extends TableImpl<ExtensionVersionRecord> {
     public final TableField<ExtensionVersionRecord, Long> EXTENSION_ID = createField(DSL.name("extension_id"), SQLDataType.BIGINT, this, "");
 
     /**
-     * The column <code>public.extension_version.published_with_id</code>.
-     */
-    public final TableField<ExtensionVersionRecord, Long> PUBLISHED_WITH_ID = createField(DSL.name("published_with_id"), SQLDataType.BIGINT, this, "");
-
-    /**
      * The column <code>public.extension_version.active</code>.
      */
     public final TableField<ExtensionVersionRecord, Boolean> ACTIVE = createField(DSL.name("active"), SQLDataType.BOOLEAN.nullable(false), this, "");
@@ -291,7 +286,7 @@ public class ExtensionVersion extends TableImpl<ExtensionVersionRecord> {
 
     @Override
     public List<Index> getIndexes() {
-        return Arrays.asList(Indexes.EXTENSION_VERSION__EXTENSION_ID__IDX, Indexes.EXTENSION_VERSION__PUBLISHED_BY_ID__IDX, Indexes.EXTENSION_VERSION__PUBLISHED_WITH_ID__IDX, Indexes.EXTENSION_VERSION_BY_TARGET_PLATFORM_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_LATEST_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_VERSION_LIST_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_VERSION_MAP_ORDER_BY_IDX);
+        return Arrays.asList(Indexes.EXTENSION_VERSION__EXTENSION_ID__IDX, Indexes.EXTENSION_VERSION__PUBLISHED_BY_ID__IDX, Indexes.EXTENSION_VERSION_BY_TARGET_PLATFORM_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_LATEST_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_VERSION_LIST_ORDER_BY_IDX, Indexes.EXTENSION_VERSION_VERSION_MAP_ORDER_BY_IDX);
     }
 
     @Override
@@ -306,7 +301,7 @@ public class ExtensionVersion extends TableImpl<ExtensionVersionRecord> {
 
     @Override
     public List<ForeignKey<ExtensionVersionRecord, ?>> getReferences() {
-        return Arrays.asList(Keys.EXTENSION_VERSION__EXTENSION_VERSION_PUBLISHED_BY_ID_FKEY, Keys.EXTENSION_VERSION__EXTENSION_VERSION_PUBLISHED_WITH_ID_FKEY, Keys.EXTENSION_VERSION__EXTENSION_VERSION_REMOVED_BY_ID_FKEY, Keys.EXTENSION_VERSION__EXTENSION_VERSION_SIGNATURE_KEY_PAIR_FKEY, Keys.EXTENSION_VERSION__FKKHS1EC9S9J08FGICQ9PMWU6BT);
+        return Arrays.asList(Keys.EXTENSION_VERSION__EXTENSION_VERSION_PUBLISHED_BY_ID_FKEY, Keys.EXTENSION_VERSION__EXTENSION_VERSION_REMOVED_BY_ID_FKEY, Keys.EXTENSION_VERSION__EXTENSION_VERSION_SIGNATURE_KEY_PAIR_FKEY, Keys.EXTENSION_VERSION__FKKHS1EC9S9J08FGICQ9PMWU6BT);
     }
 
     @Override
