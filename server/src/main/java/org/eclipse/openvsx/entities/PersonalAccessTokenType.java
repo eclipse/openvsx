65eb66687386208028eaeeec49e5b52c5bdc0b00
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2026-08-27 07:28:27 +0200

    add deprecation note and update license header

--- server/src/main/java/org/eclipse/openvsx/entities/PersonalAccessTokenType.java
+++ server/src/main/java/org/eclipse/openvsx/entities/PersonalAccessTokenType.java
@@ -1,12 +1,15 @@
-/********************************************************************************
- * Copyright (c) 2020 TypeFox and others
+/******************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0
- ********************************************************************************/
+ *****************************************************************************/
 package org.eclipse.openvsx.entities;
 
 /**
@@ -21,6 +24,7 @@ public enum PersonalAccessTokenType {
      * One time usable general personal access token.
      * Legacy: was used until 1.2.0; but is not anymore.
      */
+    @Deprecated
     OTT(true, false),
     /**
      * One time usable, trusted publishing issued access token.
